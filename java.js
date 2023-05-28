d4fe895c69902dd5e4b59df85c8f191180a3cc67
Anilchauhan24 <rewardpointsbit@gmail.com>
2023-05-28 17:23:13 +0530

    updat

---
+++ java.js
@@ -0,0 +1,27 @@
+document.getElementById('id').innerHTML='This is yout ';
+
+window.alert("Write your name");
+
+
+document.write(5+6);
+
+//window.print();/
+
+let x,y,z;
+x= 40;
+y= 50;
+z= 60;
+
+p= x+y+z;
+document.getElementById('p').innerHTML='the value of p = z+x;';
+
+
+
+let x = 16 + "Volvo";
+document.getElementById("demo").innerHTML = x;
+
+function myFunction(p1,p2){
+    return p1 * p2;
+}
+let anil= myFunction(50, 10);
+document.getElementById("lk").innerHTML= anil;
---
+++ javascript.js
@@ -0,0 +1,5 @@
+var aCal =function(b){
+    return b*b;
+}
+console.log("calling anonymius:"+ aCal(10));
+
