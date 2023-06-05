8f5383929b3280c313b766494e2c3a721a63afae
Anilchauhan24 <rewardpointsbit@gmail.com>
2023-06-05 19:52:18 +0530

    update learnjavascript.js

---
+++ learnjavascript.js
@@ -0,0 +1,21 @@
+var aCal= function(b){
+    return b*b;
+}
+console.log("value is;"+ aCal(21));
+
+
+var aCal= function(q){
+    return q*q;
+}
+console.log("value is new:"+ aCal(56));
+
+function nCalSquare(s){
+    return s*s+s;
+}
+console.log("New value is:"+ nCalSquare(12));
+
+
+var aCal = function(w){
+    return w*w;
+}
+console.log("new value:"+ aCal(20));
