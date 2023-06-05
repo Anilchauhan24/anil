1a7ab3cfa29b107f4d1e7fef6c44d69ee83c034e
Anilchauhan24 <rewardpointsbit@gmail.com>
2023-06-05 19:53:25 +0530

    learnjavascript.js

---
+++ bootstrap.js
@@ -0,0 +1,12 @@
+/* Object creating */
+/I'm going to vreate an Object in javascript/
+function createPerson(){
+
+var person = new Object();
+person.name="anil";
+person.designation="trainer";
+person.phono= 9650117292;
+return person()
+
+}
+var anil= createPerson{};
