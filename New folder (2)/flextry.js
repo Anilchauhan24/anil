e8b8a863b1b280ad0bf03f6cfb0c64a113745602
Anilchauhan24 <rewardpointsbit@gmail.com>
2023-06-15 04:55:41 +0530

    js

---
+++ New folder (2)/flextry.js
@@ -0,0 +1,46 @@
+prompt("enter your name please" );
+alert("now you're okay to go ahead");
+
+/*var today = newDate();
+var = greeting;
+if (hourNow > 18){
+    greeting= 'Good evening!';
+}
+else if(hourNow > 12){
+    greeting = 'Good afternoon';
+}
+else if(hourNow > 0){
+    greeting = 'Good morning!';
+}
+else{
+    greeting = 'Welcome to my channel';
+}
+document.write('<h3>'+ greeting+ '</h3>')*/
+
+document.write('<h1> Welcome to cnx page </h1>', 
+'<p> Do we write in paara</p>','<span> <br>yes you write in para<br></span>' );
+
+//Using a veriable to store a number//
+/*
+var price;
+var quantity;
+var total;
+
+price= 300;
+quantity = 10;
+total = price*quantity;
+
+var e1= document.getElementById('const'){
+e1.textContent='$'+total;
+}
+*/
+/*var colors;
+colors =['white','black','custom'];
+document.getElementById('color').innerHTML=[0];
+*/
+
+function area(width, height){
+  return width*height;
+}
+var size= area(3,5);
+console.log(size);
