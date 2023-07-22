2e2f8767d11a50fa6125fd523d6a6215f9c861f8
Anil chauhan <rewardpointsbit@gmail.com>
2023-07-22 10:23:44 +0530

    update

---
+++ javascript.js
@@ -0,0 +1,105 @@
+//var a = "Anil";
+//assignment value is 10 and declaration is a;//
+var b= 40;
+var c= 50;
+d= b+c;//
+console.log(c);
+console.log(typeof d);
+var e= 50.60;
+console.log(typeof e);
+
+var a="chuhan";
+console.log(typeof a);
+
+var f= true;
+console.log(typeof f);
+
+var a=50;
+var c= 60;
+ d= a*c;
+ console.log(d);
+
+ var a= 5;
+ var b= 6;
+  d=a%b;
+  console.log(d);
+
+  var a ="Anil";
+  var z="Chauhan";
+   c= a+z;
+   console.log(c);
+
+   var d="aNIL";
+   var x= "chauhan";
+    y= d*x;
+    console.log(y);
+
+    var f= "Rohin";
+     var g= "Samba";
+      h= f-g;
+      console.log(h);
+
+
+      var r= "Anil";
+      var s= 5000;
+      t =r+s;
+      console.log(t);
+
+      var t= "5000"
+      var u= 7000;
+       v= t+u;
+       console.log(v);
+
+       var b= "2555Student";
+       var c= 6000;
+       d= b+c;
+       console.log(parseInt(d))
+
+
+
+       var t= "20"+30+"20";
+       console.log(t);
+
+       //convert one data type 
+
+       var a= 50.52;
+       var b=60;
+      console.log( parseInt(a)+ parseInt(b));
+      console.log(parseFloat(a)+ parseFloat(b));
+
+
+
+      
+
+
+      var a =true;
+      var b= 1;
+      c= a+b;
+      console.log(c);
+
+      var a=7;
+      var b= "10"
+      var c= 50;
+
+      console.log(a===c);
+
+var a= 50;
+ a=60;
+ console.log(a);
+
+ var a =60;
+ var a= 70;
+ a= 80;
+ console.log(a);
+
+
+
+ let i = 50;
+  i = 40;
+ console.log(i);
+
+ 
+
+
+
+
